fe17c01795b3d2fe9b2dc8ca04d45cb06406960f
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-10 14:11:40 -0400

    Created Users Collection and Tested with POSTMAN

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -6,14 +8,19 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send("New Signup");
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("User not created");
+  }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
   console.log(email, password);
