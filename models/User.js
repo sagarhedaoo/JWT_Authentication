c8069061b08986a57da6c1ce04e500b87a20c0aa
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-10 14:37:47 -0400

    Handled errors using custom handling function

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please Enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, "Please Enter Password"],
+    minlength: [6, "Minimum Password Length is 6 Characters"],
   },
 });
 
